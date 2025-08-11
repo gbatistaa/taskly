337df7fa45a07a44d2be5e7edce89246ed29a76a
gbatistaa <gbatistadev@gmail.com>
2025-08-11 18:26:16 -0300

    feat: create a user DTO for the response object of the create user HTTP request

---
+++ backend/src/modules/user/dto/user-dto.ts
@@ -0,0 +1,27 @@
+import { Exclude, Expose } from 'class-transformer';
+import { UserInterface } from '../interfaces/user.interface';
+
+@Exclude()
+export class UserDTO implements UserInterface {
+  id: string;
+  cpf: string;
+
+  @Expose()
+  email: string;
+
+  password: string;
+
+  @Expose()
+  firstName: string;
+
+  @Expose()
+  lastName: string;
+
+  @Expose()
+  username: string;
+
+  salt: string;
+  birthDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}

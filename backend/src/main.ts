026af2c52738f7188a40fb6c1c5f864898009856
gbatistaa <gbatistadev@gmail.com>
2025-08-11 18:20:16 -0300

    feat: add globally pipe validation and swagger documentation start

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
+// Load env vars before anything else
+import * as dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swagger } from './modules/common/docs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  swagger(app, process.env.NODE_ENV || 'development');
+  await app.listen(process.env.PORT || 3000);
 }
-bootstrap();
+
+void bootstrap();

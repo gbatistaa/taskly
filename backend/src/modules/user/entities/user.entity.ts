8182333b40e106dfb860aa93d2396de8b2e5edc6
gbatistaa <gbatistadev@gmail.com>
2025-08-11 18:25:33 -0300

    feat: create the user entity and its constraints

--- backend/src/modules/user/entities/user.entity.ts
+++ backend/src/modules/user/entities/user.entity.ts
@@ -1,13 +1,36 @@
 import { CommonEntity } from 'src/modules/common/entities/common-entity';
-import { Entity } from 'typeorm';
+import { Column, Entity, Unique } from 'typeorm';
 import { UserInterface } from '../interfaces/user.interface';
 
 @Entity()
+@Unique(['cpf'])
+@Unique(['email'])
 export class User extends CommonEntity implements UserInterface {
+  @Column({ type: 'text', nullable: false })
   cpf: string;
+
+  @Column({ type: 'text', nullable: false })
   email: string;
+
+  @Column({ type: 'text', nullable: false })
   password: string;
+
+  @Column({ type: 'text', nullable: false })
   firstName: string;
+
+  @Column({ type: 'text', nullable: false })
   lastName: string;
+
+  @Column({ type: 'text', nullable: false })
   username: string;
+
+  @Column({ type: 'text', nullable: false })
+  salt: string;
+
+  @Column({
+    type: 'timestamp',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  birthDate: Date;
 }

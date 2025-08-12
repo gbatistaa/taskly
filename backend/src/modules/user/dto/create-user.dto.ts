3081395c74c4dd6cfb8fe9839ada95c848fdb7a1
gbatistaa <gbatistadev@gmail.com>
2025-08-12 00:23:35 -0300

    feat: add validation pipes in all props and add cpf, password, username and birthDate props

--- backend/src/modules/user/dto/create-user.dto.ts
+++ backend/src/modules/user/dto/create-user.dto.ts
@@ -1,8 +1,26 @@
+import { IsDateString, IsEmail, IsString, Length } from 'class-validator';
 import { CreatableUserInterface } from '../interfaces/creatable-user.interface';
 
 export class CreateUserDto implements CreatableUserInterface {
-  username: string;
+  @IsString()
+  @Length(11, 11)
+  cpf: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
   firstName: string;
+
+  @IsString()
   lastName: string;
+
+  @IsString()
+  username: string;
+
+  @IsDateString()
+  birthDate: Date;
 }

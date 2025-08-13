065e2c03f33dae0881a536722c5e1a1853e82b5d
gbatistaa <gbatistadev@gmail.com>
2025-08-12 21:35:08 -0300

    feat: import the JWT module on the authentication module

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AuthController],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}

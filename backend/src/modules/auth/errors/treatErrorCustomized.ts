9dda9523de990e200e6b5a92f7a950a87697384d
gbatistaa <gbatistadev@gmail.com>
2025-08-12 21:35:37 -0300

    feat: create a function for customized error treatment

---
+++ backend/src/modules/auth/errors/treatErrorCustomized.ts
@@ -0,0 +1,22 @@
+import { HttpException } from '@nestjs/common';
+import { postgresErrorMap } from 'src/modules/common/utils/postgresErrorMap';
+
+export const treatKnownErrors = (error: unknown) => {
+  // Treat Http exception errors:
+  if (error instanceof HttpException) {
+    throw error;
+  }
+
+  // Treat database errors:
+  if (
+    typeof error === 'object' &&
+    error !== null &&
+    'code' in error &&
+    typeof (error as { code: unknown }).code === 'string' &&
+    postgresErrorMap[(error as { code: string }).code]
+  ) {
+    throw postgresErrorMap[(error as { code: string }).code](
+      (error as { detail?: string }).detail,
+    );
+  }
+};

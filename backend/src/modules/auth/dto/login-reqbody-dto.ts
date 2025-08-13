728de5587ebd989680536caa3dc8f4881264ce2f
gbatistaa <gbatistadev@gmail.com>
2025-08-12 21:59:03 -0300

    chore: add validation pipes on the login request body dto properties

--- backend/src/modules/auth/dto/login-reqbody-dto.ts
+++ backend/src/modules/auth/dto/login-reqbody-dto.ts
@@ -1,6 +1,11 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
 import { LoginRequestBodyInterface } from '../interfaces/login-reqbody.interface';
 
 export class LoginRequestBodyDTO implements LoginRequestBodyInterface {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
 }

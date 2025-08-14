38bdb82f553c5846d3f0127b8cde03e85a86917f
gbatistaa <gbatistadev@gmail.com>
2025-08-13 22:27:12 -0300

    feat: create the refresh token entity class

--- backend/src/modules/refresh-token/entities/refresh-token.entity.ts
+++ backend/src/modules/refresh-token/entities/refresh-token.entity.ts
@@ -1,1 +1,16 @@
-export class RefreshToken {}
+import { CommonEntity } from 'src/modules/common/entities/common-entity';
+import { User } from 'src/modules/user/entities/user.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { RefreshTokenInterface } from '../interfaces/refresh-token.interface';
+
+@Entity()
+export class RefreshToken
+  extends CommonEntity
+  implements RefreshTokenInterface
+{
+  @ManyToOne(() => User, (user) => user.id)
+  user_id: string;
+
+  @Column({ type: 'citext', nullable: false })
+  token_hash: string;
+}

e12aef1cebfa7ef23612b263fa067e5bcc212ac4
gbatistaa <gbatistadev@gmail.com>
2025-08-14 21:45:45 -0300

    feat: import the refresh token module to the authentication module

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { RefreshTokenModule } from '../refresh-token/refresh-token.module';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -7,6 +8,7 @@ import { AuthService } from './auth.service';
 @Module({
   imports: [
     UserModule,
+    RefreshTokenModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
     }),

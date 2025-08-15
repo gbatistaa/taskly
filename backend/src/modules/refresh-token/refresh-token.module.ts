be369e603bf7ecc91fecd7514e955e05cde92c5c
gbatistaa <gbatistadev@gmail.com>
2025-08-14 21:48:41 -0300

    feat: import the TypeORM refresh token entity class on its module and export de refresh token service class

--- backend/src/modules/refresh-token/refresh-token.module.ts
+++ backend/src/modules/refresh-token/refresh-token.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { RefreshTokenService } from './refresh-token.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RefreshToken } from './entities/refresh-token.entity';
 import { RefreshTokenController } from './refresh-token.controller';
+import { RefreshTokenService } from './refresh-token.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RefreshToken])],
   controllers: [RefreshTokenController],
   providers: [RefreshTokenService],
+  exports: [RefreshTokenService],
 })
 export class RefreshTokenModule {}

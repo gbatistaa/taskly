98c07c04c16642c3bf79efebe1040b00c83ad6a4
gbatistaa <gbatistadev@gmail.com>
2025-08-14 23:28:30 -0300

    feat: use the cookie parser middleware globally

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,14 @@ dotenv.config({ path: './.env' });
 
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { swagger } from './modules/common/docs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   app.useGlobalPipes(
     new ValidationPipe({

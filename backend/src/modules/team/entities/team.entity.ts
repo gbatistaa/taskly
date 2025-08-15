d110beb31b66428f771165ca8bbd434fb16481a0
gbatistaa <gbatistadev@gmail.com>
2025-08-15 09:54:00 -0300

    feat: create the structure on the team entity

--- backend/src/modules/team/entities/team.entity.ts
+++ backend/src/modules/team/entities/team.entity.ts
@@ -1,1 +1,20 @@
-export class Team {}
+import { CommonEntity } from 'src/modules/common/entities/common-entity';
+import { User } from 'src/modules/user/entities/user.entity';
+import { Column, Entity, JoinTable, OneToOne } from 'typeorm';
+import { TeamInterface } from '../interfaces/team.interface';
+
+@Entity()
+export class Team extends CommonEntity implements TeamInterface {
+  @OneToOne(() => User, (owner) => owner.id)
+  @JoinTable()
+  ownerId: string;
+
+  @Column({ type: 'citext', nullable: false })
+  name: string;
+
+  @Column({ type: 'citext', nullable: true })
+  company: string;
+
+  @Column({ type: 'citext', nullable: false })
+  description: string;
+}

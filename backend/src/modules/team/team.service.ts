f3f52318aadc45b92f56865c905a65d12d66540a
gbatistaa <gbatistadev@gmail.com>
2025-08-17 21:02:19 -0300

    feat: create the create team api function

--- backend/src/modules/team/team.service.ts
+++ backend/src/modules/team/team.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { plainToInstance } from 'class-transformer';
+import { Repository } from 'typeorm';
 import { CreateTeamDto } from './dto/create-team.dto';
+import { TeamDTO } from './dto/team.dto';
 import { UpdateTeamDto } from './dto/update-team.dto';
+import { Team } from './entities/team.entity';
 
 @Injectable()
 export class TeamService {
-  create(createTeamDto: CreateTeamDto) {
-    return 'This action adds a new team';
+  constructor(@InjectRepository(Team) private repo: Repository<Team>) {}
+
+  async create(createTeamDto: CreateTeamDto): Promise<TeamDTO> {
+    const team = this.repo.create(createTeamDto);
+    const dbTeam = await this.repo.save(team);
+
+    return plainToInstance(TeamDTO, dbTeam);
   }
 
   findAll() {

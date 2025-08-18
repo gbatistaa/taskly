5e4f85fabed2e48ede60e821301a9e65e0a04f3b
gbatistaa <gbatistadev@gmail.com>
2025-08-17 21:01:07 -0300

    feat: import the team entity and user module dependencies, and export the team service

--- backend/src/modules/team/team.module.ts
+++ backend/src/modules/team/team.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TeamService } from './team.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '../user/user.module';
+import { Team } from './entities/team.entity';
 import { TeamController } from './team.controller';
+import { TeamService } from './team.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Team]), UserModule],
   controllers: [TeamController],
   providers: [TeamService],
+  exports: [TeamService],
 })
 export class TeamModule {}

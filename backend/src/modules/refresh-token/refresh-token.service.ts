db7cffb89d8210619a2e6035a3074724c2154ea7
gbatistaa <gbatistadev@gmail.com>
2025-08-17 21:40:54 -0300

    feat: create the search functions for the refresh token service

--- backend/src/modules/refresh-token/refresh-token.service.ts
+++ backend/src/modules/refresh-token/refresh-token.service.ts
@@ -6,8 +6,9 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { plainToInstance } from 'class-transformer';
 import { Repository } from 'typeorm';
-import { treatKnownErrors } from '../auth/errors/treatErrorCustomized';
+import { treatKnownErrors } from '../common/errors/treatErrorCustomized';
 import { CreateRefreshTokenDto } from './dto/create-refresh-token.dto';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 import { RefreshToken } from './entities/refresh-token.entity';
 
 @Injectable()
@@ -31,15 +32,34 @@ export class RefreshTokenService {
     }
   }
 
-  findAll() {
-    return `This action returns all refreshToken`;
+  async findAll(): Promise<RefreshTokenDto[]> {
+    try {
+      const tokens = await this.repo.find();
+      if (!tokens) {
+        throw new NotFoundException('The refresh tokens table is empty');
+      }
+      const tokensDtos = tokens.map((token) => {
+        return plainToInstance(RefreshTokenDto, token);
+      });
+
+      return tokensDtos;
+    } catch (error: unknown) {
+      treatKnownErrors(error);
+      throw new InternalServerErrorException();
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} refreshToken`;
+  async findOne(userId: string): Promise<RefreshTokenDto> {
+    try {
+      const token = await this.repo.findOne({ where: { userId: userId } });
+      return plainToInstance(RefreshTokenDto, token);
+    } catch (error) {
+      treatKnownErrors(error);
+      throw new InternalServerErrorException();
+    }
   }
 
-  async remove(userId: string) {
+  async remove(userId: string): Promise<RefreshTokenDto> {
     try {
       const tokenToRemove = await this.repo.findOne({
         where: { userId: userId },
@@ -49,7 +69,8 @@ export class RefreshTokenService {
         throw new NotFoundException('Token does not exist');
       }
 
-      await this.repo.remove(tokenToRemove);
+      const removed = await this.repo.remove(tokenToRemove);
+      return plainToInstance(RefreshTokenDto, removed);
     } catch (error) {
       treatKnownErrors(error);
 

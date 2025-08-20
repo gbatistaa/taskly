3488cd0416aafb979339d4bca7b9ef7a366929f4
gbatistaa <gbatistadev@gmail.com>
2025-08-19 23:48:57 -0300

    feat: create the user logout route

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -4,11 +4,13 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Req,
   Res,
 } from '@nestjs/common';
 import type { Response } from 'express';
 import { AuthService } from './auth.service';
 import { LoginRequestBodyDTO } from './dto/login-reqbody-dto';
+import { type LogoutRequest } from './interfaces/logout-request.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +20,11 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: LoginRequestBodyDTO, @Res() res: Response) {
     return await this.authService.login(loginDto, res);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('logout')
+  logout(@Req() req: LogoutRequest, @Res() res: Response) {
+    return this.authService.logout(req, res);
   }
 }

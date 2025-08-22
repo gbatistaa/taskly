a552fc2be3c2c9858422a2d1a303dc22b7459c74
gbatistaa <gbatistadev@gmail.com>
2025-08-22 17:27:18 -0300

    feat: finish the jwt token expired error treatment logic

--- backend/src/modules/auth/middlewares/token-validation/token-validation.middleware.ts
+++ backend/src/modules/auth/middlewares/token-validation/token-validation.middleware.ts
@@ -2,16 +2,22 @@ import {
   Injectable,
   InternalServerErrorException,
   NestMiddleware,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { JwtService, TokenExpiredError } from '@nestjs/jwt';
 import { NextFunction, type Response } from 'express';
 import { treatKnownErrors } from 'src/modules/common/errors/treatErrorCustomized';
+import { RefreshTokenService } from 'src/modules/refresh-token/refresh-token.service';
 import { AccessTokenPayload } from '../../interfaces/access-token-payload.interface';
 import { RefreshRequest } from '../../interfaces/refresh-request.interface';
+import { generateAccessToken } from '../../tokens/generate-access-token';
 
 @Injectable()
 export class TokenValidationMiddleware implements NestMiddleware {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private refreshTokenService: RefreshTokenService,
+  ) {}
 
   async use(req: RefreshRequest, res: Response, next: NextFunction) {
     // Global middleware to validate request access token:
@@ -21,18 +27,54 @@ export class TokenValidationMiddleware implements NestMiddleware {
     try {
       const payload: AccessTokenPayload = await this.jwtService.verifyAsync(
         accessToken,
-        {
-          secret: process.env.JWT_SECRET,
-        },
+        { secret: process.env.JWT_SECRET },
       );
-      console.log(payload);
+
+      // console.log(payload);
+      req.payload = payload;
 
       next();
     } catch (error: unknown) {
+      // In case of token expired:
+      if (error instanceof TokenExpiredError) {
+        try {
+          const payload: AccessTokenPayload =
+            await this.jwtService.decode(accessToken);
+
+          console.log(payload);
+
+          // Trying to find a refresh token of the payload user on the database
+          const refreshTokenFound = await this.refreshTokenService.findOne(
+            'userId',
+            payload.id,
+          );
+
+          if (!refreshTokenFound) {
+            throw new UnauthorizedException(
+              'The user refresh token does not exist in the database',
+            );
+          }
+
+          const [, newAccessToken] = await generateAccessToken(
+            payload,
+            this.jwtService,
+            res,
+          );
+
+          return res.json({
+            message: 'The access token was refreshed',
+            newAccessToken,
+            statusCode: 200,
+          });
+        } catch (error: unknown) {
+          console.log(error);
+        }
+      }
+
       treatKnownErrors(error);
 
       throw new InternalServerErrorException(
-        'Unexpected error on access token validation',
+        'Unexpected error on the access token validation middleware',
       );
     }
   }

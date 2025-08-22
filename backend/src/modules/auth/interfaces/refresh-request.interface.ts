ee3b35f9be876f201bf34d539964c409dc306b0b
gbatistaa <gbatistadev@gmail.com>
2025-08-22 17:26:44 -0300

    feat: add the access token payload on the rfreshtoken rout request

--- backend/src/modules/auth/interfaces/refresh-request.interface.ts
+++ backend/src/modules/auth/interfaces/refresh-request.interface.ts
@@ -1,7 +1,10 @@
 import { Request } from 'express';
+import { AccessTokenPayload } from './access-token-payload.interface';
 
 export interface RefreshRequest extends Request {
   cookies: {
     accessToken: string;
   };
+
+  payload?: AccessTokenPayload;
 }

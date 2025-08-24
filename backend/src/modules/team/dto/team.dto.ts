6c58966ec20a8a6607410fd28a2750cc1cda40ca
gbatistaa <gbatistadev@gmail.com>
2025-08-24 12:41:40 -0300

    feat: expose the props from the team dto class

--- backend/src/modules/team/dto/team.dto.ts
+++ backend/src/modules/team/dto/team.dto.ts
@@ -1,7 +1,14 @@
+import { Exclude, Expose } from 'class-transformer';
 import { TeamInterface } from '../interfaces/team.interface';
 
+@Exclude()
 export class TeamDTO implements Partial<TeamInterface> {
+  @Expose()
   name: string;
+
+  @Expose()
   company: string;
+
+  @Expose()
   description: string;
 }

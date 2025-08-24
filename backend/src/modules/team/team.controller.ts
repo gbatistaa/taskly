4f81892c262031c1494dc029220efd720b3536a2
gbatistaa <gbatistadev@gmail.com>
2025-08-24 12:40:22 -0300

    feat: create the create team and find all teams operations

--- backend/src/modules/team/team.controller.ts
+++ backend/src/modules/team/team.controller.ts
@@ -1,14 +1,6 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { type RefreshRequest } from '../auth/interfaces/refresh-request.interface';
 import { CreateTeamDto } from './dto/create-team.dto';
-import { UpdateTeamDto } from './dto/update-team.dto';
 import { TeamService } from './team.service';
 
 @Controller('team')
@@ -16,27 +8,27 @@ export class TeamController {
   constructor(private readonly teamService: TeamService) {}
 
   @Post('create')
-  create(@Body() createTeamDto: CreateTeamDto) {
-    return this.teamService.create(createTeamDto);
+  create(@Body() createTeamDto: CreateTeamDto, @Req() req: RefreshRequest) {
+    return this.teamService.create(createTeamDto, req);
   }
 
-  @Get()
+  @Get('find')
   findAll() {
     return this.teamService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.teamService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.teamService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTeamDto: UpdateTeamDto) {
-    return this.teamService.update(+id, updateTeamDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateTeamDto: UpdateTeamDto) {
+  //   return this.teamService.update(+id, updateTeamDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.teamService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.teamService.remove(+id);
+  // }
 }

e72f2348da320482eb48752789693feece7c13d7
gbatistaa <gbatistadev@gmail.com>
2025-10-31 10:43:00 -0300

    feat: import the auhtentication module on the team member module

--- backend/src/modules/team-member/team-member.module.ts
+++ backend/src/modules/team-member/team-member.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 import { TeamMember } from './entities/team-member.entity';
 import { TeamMemberController } from './team-member.controller';
 import { TeamMemberService } from './team-member.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TeamMember])],
+  imports: [TypeOrmModule.forFeature([TeamMember]), AuthModule],
   controllers: [TeamMemberController],
   providers: [TeamMemberService],
   exports: [TeamMemberService],

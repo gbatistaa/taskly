a42532e9a0b8eaae27e3f643f5e3e4f08fac0dcd
gbatistaa <gbatistadev@gmail.com>
2025-11-04 20:01:27 -0300

    feat: add axios API configuration with default settings

---
+++ frontend/app/api/api.ts
@@ -0,0 +1,13 @@
+// api.js
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;

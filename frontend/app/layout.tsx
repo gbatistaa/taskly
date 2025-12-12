50233d885440c12edfc2219beb99df86a51b2de1
gbatistaa <gbatistadev@gmail.com>
2025-12-12 17:09:41 -0300

    feat: add Toaster component for notifications and update package dependencies

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
         <div className="flex flex-col gap-3 max-h-screen h-screen box-border items-center justify-center bg-slate-950 font-sans">
           {children}
         </div >
